25623d02c78ec7ba8bf3b9c9e2b996fc1265a76d
Florian Klampfer <f.klampfer@gmail.com>
2011-11-15 14:59:42 +0100

    shortcuts

--- js/wisp.js
+++ js/wisp.js
@@ -1,4 +1,4 @@
-var $page, $weak, $icon, supports_local_storage, clockIntervals = [];
+var $page, $weak, $icon, supports_local_storage, clockIntervals = [], pwIndex = 0, shift=false;
 
 $(document).ready(function() {
 	// check for localStorage support
@@ -17,11 +17,126 @@ $('.ui-page').live('pagecreate',function(event) {
 	
 	// Resets the app
 	reset();
-		
-	// Hide the "More..."-Button after it has been clicked
-	/*$('.more .ui-collapsible-heading').bind('vclick', function() {
-		$(this).hide();
-	});*/
+
+	$page.find('.strong').bind('vclick',function() {
+		$(this).selectText();
+		document.execCommand('Copy');
+	});
+
+	$page.unbind('keydown').bind('keydown',function(e) {
+		if($page.find('input:focus').length == 0) {
+			if(e.keyCode == '27') {
+				e.preventDefault();
+				$page.find('.logo a').focus();
+			}
+			// j
+			if(e.keyCode == '74') {
+				e.preventDefault();
+				pwIndex++; 
+				if(pwIndex >= 8) pwIndex = 0;
+				$strong = $page.find('.strong-'+pwIndex);
+				
+				var os = $strong.offset().top;
+				var h = $(window).height()-100;
+				if(os > h) {
+					$(window).scrollTop(os - h);
+				}
+
+				if(pwIndex >= 2)
+					$('.more .ui-collapsible-heading').trigger('expand');
+
+				$strong.selectText();
+				document.execCommand('Copy');
+			}
+			// k
+			else if(e.keyCode == '75') {
+				e.preventDefault();
+				if(pwIndex <= 0) pwIndex = 8;
+				pwIndex--;
+				$strong = $page.find('.strong-'+pwIndex);
+
+				var os = $strong.offset().top;
+				var h = $(window).height()-100;
+				if(os > h) {
+					$(window).scrollTop(os - h);
+				}
+
+				if(pwIndex >= 3)
+					$('.more .ui-collapsible-heading').trigger('expand');
+
+				$strong.selectText();
+				document.execCommand('Copy');
+			}
+			// shift
+			else if(e.keyCode == '16') {
+				shift = true;
+			}
+			// a
+			else if(e.keyCode == '65' && shift == false) {
+				e.preventDefault();
+				var $t = $page.find('.chars-0');
+				$t.attr('checked',!$t.attr('checked')).checkboxradio('refresh').trigger('change');
+			}
+			// A
+			else if(e.keyCode == '65' && shift == true) {
+				e.preventDefault();
+				var $t = $page.find('.chars-1');
+				$t.attr('checked',!$t.attr('checked')).checkboxradio('refresh').trigger('change');
+			}
+			// 1
+			else if(e.keyCode == '49' && shift == false) {
+				e.preventDefault();
+				var $t = $page.find('.chars-2');
+				$t.attr('checked',!$t.attr('checked')).checkboxradio('refresh').trigger('change');
+			}
+			// !
+			else if(e.keyCode == '49' && shift == true) {
+				e.preventDefault();
+				var $t = $page.find('.chars-3');
+				$t.attr('checked',!$t.attr('checked')).checkboxradio('refresh').trigger('change');
+			}
+			// h
+			else if(e.keyCode == '72') {
+				e.preventDefault();
+				$c = $page.find('.length:checked');
+				if($c.parents('.ui-radio').prev().length) {
+					$page.find('.length').attr('checked',false).checkboxradio('refresh');
+					$c.parents('.ui-radio').prev().find('.length').attr('checked',true).checkboxradio('refresh').trigger('change');
+				}
+			}
+			// l
+			else if(e.keyCode == '76') {
+				e.preventDefault();
+				$c = $page.find('.length:checked');
+				if($c.parents('.ui-radio').next().length) {
+					$page.find('.length').attr('checked',false).checkboxradio('refresh');
+					$c.parents('.ui-radio').next().find('.length').attr('checked',true).checkboxradio('refresh').trigger('change');
+				}
+			}
+			// x, d
+			else if(e.keyCode == '88' || e.keyCode == '68') {
+				e.preventDefault();
+				reset();
+			}
+			// i
+			else if(e.keyCode == '73') {
+				e.preventDefault();
+				$weak.focus();
+			}
+			// m
+			else if(e.keyCode == '77') {
+				$h = $page.find('.more .ui-collapsible-heading');
+				if($h.hasClass('ui-collapsible-heading-collapsed'))
+					$h.trigger('expand'); 
+				else 
+					$h.trigger('collapse');
+			}
+		}
+	});		
+
+	$page.unbind('keyup').bind('keyup',function(e) {
+		shift = false;
+	});
 	
 	// AJAX request by clicking on the lock
 	$icon.bind('vclick', function() {
@@ -58,6 +173,8 @@ $('.ui-page').live('pagecreate',function(event) {
 			v = $(this).data('v');
 			$(this).html(v.substring(0,to));
 		});
+		$page.find('.strong-'+pwIndex).selectText();
+		document.execCommand('Copy');
 	});
 	
 	$page.find('.chars').bind('change', function() {
@@ -237,6 +354,10 @@ showPasswords = function(data) {
 		$page.find('.strong-'+_i).removeClass('strong-placeholder').data('v',s).html(s.substring(0,to));
 	}
 	
+	$page.find('.logo a').focus();
+	$page.find('.strong-'+pwIndex).selectText();
+	document.execCommand('Copy');
+
 	// Automatically remove password after 60 sec to prevent abuse
 	$clock = $page.find('.clock');
 	
@@ -292,6 +413,8 @@ reset = function() {
 		$(this).addClass('strong-placeholder').data('v','').html($(this).data('placeholder'));
 	});
 	
+	pwIndex = 0;
+
 	$weak.focus();
 };
 
@@ -317,4 +440,22 @@ setStatus = function(status) {
 	}
 };
 	
-
+jQuery.fn.selectText = function(){
+    var doc = document;
+    var element = this[0];
+    if (doc.body.createTextRange) {
+        var range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        var selection = window.getSelection();
+        if (selection.setBaseAndExtent) {
+            selection.setBaseAndExtent(element, 0, element, 1);
+        } else {
+            var range = document.createRange();
+            range.selectNodeContents(element);
+            selection.removeAllRanges();
+            selection.addRange(element);
+        }
+    }
+};
